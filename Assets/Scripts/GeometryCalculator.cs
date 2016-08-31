9155f549ddc9c6fe8e860da76c4656c504e25101
kkostenkov <kkostenkov@gmail.com>
2016-08-31 18:02:27 +0400

    add (input UI)

---
+++ Assets/Scripts/GeometryCalculator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class GeometryCalculator {
+
+    /// <summary>
+    /// l = 2 * Pi * r
+    /// </summary>
+    /// <param name="radius">Circle radius</param>
+	public float CalculateCirclePerimeter(float radius)
+    {
+        if (radius <= 0) throw new ArgumentException("Radius should be greater than 0.");
+        float result = 2 * (float)Math.PI * radius;
+        return result;
+    }
+}
---
+++ Assets/Scripts/UI/CalculateButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CalculateButton : MonoBehaviour {
+    [SerializeField] Text radiusInputField;
+    [SerializeField] Text chordeInputField;
+    private float radius;
+    private ulong chordeLength;
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+
+    public void OnButtonPressed()
+    {
+        float.TryParse(radiusInputField.text, out radius);
+        ulong.TryParse(chordeInputField.text, out chordeLength);
+        print(radius);
+        print(chordeLength);
+    }
+}
