4577e2b3f128bc066a0da57c82847a35867461f3
kkostenkov <kkostenkov@gmail.com>
2016-09-02 00:52:40 +0300

    feat (Grumpy)

--- Assets/Scripts/SceneDirector.cs
+++ Assets/Scripts/SceneDirector.cs
@@ -6,6 +6,7 @@ using System;
 public class SceneDirector : MonoBehaviour {
     [SerializeField] private AnswerDrawer answerDrawer;
     [SerializeField] private Text result;
+    [SerializeField] private Text grumpy;
     private static string answerTemplate = "Circle with radius {0} can hold maximum {1} chordes with offset of {2}";
 
     private static SceneDirector instance;
@@ -24,6 +25,7 @@ public class SceneDirector : MonoBehaviour {
     public void ReportResult(ChordePacker result)
     {
         ShowText(String.Format(answerTemplate, result.Radius, result.SetsPackedCount, result.OffsetLength));
+        grumpy.text = result.SetsPackedCount.ToString();
         answerDrawer.DrawChords(result);
     }
 	
@@ -35,10 +37,5 @@ public class SceneDirector : MonoBehaviour {
     internal void Print(string toPrint)
     {
         Debug.Log(toPrint);
-    }
-
-    private void OnCalculationFinished()
-    {
-
     }
 }
